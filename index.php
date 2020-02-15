c1c3baca022ef8c3e744ca0c77e8075454cf5145
sirmommam <sittipong.pu@gmail.com>
2020-02-15 10:45:57 +0700

    commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php
-/**
+<?php   
+/** test
  * CodeIgniter
  *
  * An open source application development framework for PHP
@@ -55,7 +55,7 @@
  */
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
-/*
+/*    
  *---------------------------------------------------------------
  * ERROR REPORTING
  *---------------------------------------------------------------
